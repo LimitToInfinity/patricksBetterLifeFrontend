0e9afce1c747743edb83a328e4d90e68801471d6
LimitToInfinity <21stLimitless@gmail.com>
2020-01-09 10:06:46 -0700

    Displaying events.

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
 document.addEventListener("DOMContentLoaded", postLoad);
 
+const baseURL = "http://localhost:3000/"
+const usersURL = "http://localhost:3000/users/"
+const eventsURL = "http://localhost:3000/events/"
+
 function postLoad() {
-    return;
+    fetch(eventsURL)
+        .then(parseJSON)
+        .then(extractData)
+        .then(events => events.map(displayEvent));
+}
+
+function displayEvent(event) {
+    const eventCard = document.createElement("div");
+    eventCard.classList.add("event");
+    eventCard.dataset.eventId = event.id;
+
+    const eventName = document.createElement("h2");
+    const eventDescription = document.createElement("p");
+
+    eventName.textContent = event.name;
+    eventDescription.textContent = event.description;
+
+    eventCard.append(eventName, eventDescription, deleteButton);
+
+    eventsSection.append(eventCard);
+}
+
+function extractData(object) {
+    return object.data.map(unNest);
+}
+
+function unNest(element) {
+    return element.attributes;
+}
+
+function parseJSON(response) {
+    return response.json();
+}
+
+function fetchCall(url, method, body) {
+    const headers = { "Content-Type": "application/json" }
+    return fetch(url, { method, headers, body })
 }
